ecba4948aa21f2bcc06bf615eb7f49de9bb2ed52
Lvl4Sword <Lvl4Sword@users.noreply.github.com>
2021-07-25 04:16:38 -0600

    re-add config.py

---
+++ Explorer/config.py
@@ -0,0 +1,17 @@
+# Autodetection
+autodetect_coin = True
+autodetect_config = True
+autodetect_rpc = True
+autodetect_tables = True
+
+# Coin specific
+coin_name = "CryptocurrencyExplorerCoin"
+rpcpassword = None
+rpcport = None
+rpcuser = None
+
+# Flask specific
+app_key = rb"""app_key"""
+csrf_key = "csrf_key"
+database_uri = 'postgresql://postgres:db_password@localhost/db'
+program_name = 'Cryptocurrency Explorer'
