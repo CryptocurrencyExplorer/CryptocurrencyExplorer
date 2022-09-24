6c4beeea4b64331f818d3cc4dde6efe9f7b48360
Lvl4Sword <Lvl4Sword@github.com>
2022-09-23 23:02:39 -0600

    app_key/csrf_key generator switched to upper/lower/digits.

--- Explorer/config.py
+++ Explorer/config.py
@@ -15,14 +15,11 @@ database_uri = "postgresql://postgres:db_password@localhost/db"
 program_name = "Cryptocurrency Explorer"
 
 # For a randomized app_key or csrf_key, you can use the following:
-# import secrets
-# a = "€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±º¹²³´µ¶"
-# b = "·¸»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéê"
-# c = "ëìíîïðñòóôõö÷øùúûüýþÿ0123456789abcdefghijklmnopqrs"
-# d = "tuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./"
-# e = ":;<=>?@[\\]^_`{|}~"
-# alphabet = a + b + c + d + e
-# multiplier = secrets.choice(range(1, 16))
-# the_length = 64 * multiplier
-# new = [secrets.choice(alphabet) for x in range(1, the_length)]
-# print(''.join(new))
+"""
+import secrets
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+multiplier = secrets.choice(range(1, 16))
+the_length = 64 * multiplier
+new = [secrets.choice(alphabet) for x in range(1, the_length)]
+print(''.join(new))
+"""
